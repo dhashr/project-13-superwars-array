0fdce56970f98dd1eb29bf524f7b6482f6408dc7
dhashr <kalidashramachandran@gmail.com>
2021-08-09 16:05:24 +0530

    Commit Message

--- src/app.js
+++ src/app.js
@@ -22,14 +22,38 @@ const PLAYERS = [
 ];
 
 // initialize players with image and strength
+// const initPlayers = (players) => {
+//     let detailedPlayers = '';
+
+//     // Instead of forloop use Map method
+//     // Code here
+     
+//         detailedPlayers = players.map((player,i) =>{
+//          return   {
+//                 name: player,
+//                 strength: getRandomStrength(),
+//                 img:`images/super-${i + 1}.png`,
+//                 type: (i%2 == 0) ? 'hero' :'villain'
+//             }
+        
+//         });
+    
+
+//     return detailedPlayers;
+// }
 const initPlayers = (players) => {
-    let detailedPlayers = '';
-
-    // Instead of forloop use Map method
-    // Code here
-
+    let detailedPlayers = [];
+    players.map((player, i) => {
+    let character = {};
+    character.name = player;
+    character.strength = getRandomStrength();
+    character.image = `images/super-${i + 1}.png`;
+    character.type =  (i%2 == 0) ? 'hero' :'villain';
+    detailedPlayers.push(character);
+    });
+    
     return detailedPlayers;
-}
+    } 
 
 // getting random strength
 const getRandomStrength = () => {
@@ -43,6 +67,12 @@ const buildPlayers = (players, type) => {
     // Instead of using for loop
     // Use chaining of Array methods - filter, map and join
     // Type your code here
+    fragment = players.filter(player => player.type == type).map(player =>
+         `<div class="player">
+                    <img src="${player.image}" alt = "">
+                    <div class="name">${player.name}</div>
+                    <div class="strength">${player.strength}</div>
+                </div>`).join();
 
     return fragment;
 }
